ab08e4d5e24260ce41dc62836a6a11671bec95eb
Dmitry Plevkov <wwarne@gmail.com>
2019-10-27 17:03:52 +0300

    Translate all to English

--- common_tools.py
+++ common_tools.py
@@ -14,14 +14,14 @@ async def open_chat_connection(
     delay: int = 3,
 ) -> Tuple[StreamReader, StreamWriter]:
     """
-    Устанавливает соединение с сервером чата.
+    Establish connection with a chat server.
 
-    При неудачной попытке соединения пробует переподключиться.
+    It will try to reconnect after failing attempt.
 
-    :param host: Адрес сервера
-    :param port: Порт сервера
-    :param attempts: Количество попыток соединения перед увеличением задержки перед повторным соединением
-    :param delay: Задержка перед повторным соединением, сек.
+    :param host: Chat server address
+    :param port: Chat server port
+    :param attempts: How many times to try before using delays
+    :param delay: Delay before retry, seconds.
     """
     total_attempts = 0
     while True:
@@ -29,14 +29,14 @@ async def open_chat_connection(
             reader, writer = await asyncio.wait_for(
                 asyncio.open_connection(host, port), timeout=60,
             )
-            logging.info('Установлено соединение с сервером чата')
+            logging.info(f'Connection to the hat server {host}:{port} has been established.')
             return reader, writer
         except (ConnectionRefusedError, asyncio.TimeoutError, socket.gaierror):
             if total_attempts < attempts:
-                logging.error('Нет соединения, повторная попытка')
+                logging.error('Connection error, retrying...')
                 total_attempts += 1
             else:
-                logging.error(f'Нет соединения. Повторная попытка через {delay} сек.')
+                logging.error(f'Connection error. Retrying after {delay} sec.')
                 await asyncio.sleep(delay)
 
 
@@ -48,83 +48,83 @@ async def connect_to_chat(
     delay: int = 3,
 ) -> Tuple[StreamReader, StreamWriter]:
     """
-    Контекстный менеджер, возвращающий reader&writer к чату.
+    Context manager to provide reader&writer from chat connection.
 
-    :param host: Адрес сервера
-    :param port: Порт сервера
-    :param attempts:  Количество попыток соединения перед увеличением задержки перед повторным соединением
-    :param delay: Задержка перед повторным соединением, сек.
+    :param host: Chat server address
+    :param port: Chat server port
+    :param attempts:  How many times to try before using delays
+    :param delay: Delay before retry, seconds
     """
     reader, writer = None, None
     try:
-        logging.info('Пытаемся подключиться к серверу чата')
+        logging.info('Trying to connect to the server.')
         reader, writer = await open_chat_connection(host, port, attempts, delay)
         yield reader, writer
     finally:
-        logging.info('Закрываем соединение с чатом')
+        logging.info('Closing connection to the server')
         if writer:
             writer.close()
             await writer.wait_closed()
 
 
 async def read_line_from_chat(reader: StreamReader) -> str:
-    """Получает байтовую строку и переводит её в обычную."""
+    """Grabs bytes string from connection and decode it into text."""
     chat_data = await reader.readline()
     try:
         return chat_data.decode(encoding='utf-8').strip()
     except (SyntaxError, UnicodeDecodeError):
-        logging.error('Получено ошибочное сообщение', exc_info=True)
+        logging.error('Got message that can not be decoded', exc_info=True)
         return ''
 
 
 def sanitize_message(message: str) -> str:
-    """
-    Очищает сообщение от символов переноса строки.
+    r"""
+    Clears message from `new line` symbols.
 
-    В протоколе взаимодействия с сервером знак переноса строки \n обозначает конец сообщения.
+    new line symbol (`\\n`) means an end of the message as stated in chat protocol specification.
     """
     return message.replace('\r', '').replace('\n', ' ').strip()
 
 
 async def write_line_to_chat(writer: StreamWriter, message: str) -> None:
-    """Кодирует и отправляет строку в путешествие в сторону чата."""
+    """Encode message and send it to the server."""
     message = sanitize_message(message).encode(encoding='utf-8') + b'\n'
     writer.write(message)
     await writer.drain()
-    logging.debug(f'Отправили сообщение {message}')
+    logging.debug(f'Send a message: {message}')
 
 
 async def authorise(reader: StreamReader, writer: StreamWriter, token: str) -> Optional[str]:
-    """Процесс авторизации пользователя в чате."""
-    logging.debug('Пробуем авторизоваться')
+    """Authorisation process."""
+    logging.debug('Trying to authorise')
     greetings = await read_line_from_chat(reader)
-    logging.debug(f'Приветствие чата: {greetings}')
+    logging.debug(f'Greetings from the chat: {greetings}')
     await write_line_to_chat(writer, token)
     response = await read_line_from_chat(reader)
     response = json.loads(response)
     if not response:
-        logging.error('Неправильный токен или сервер не работает')
+        logging.error('Wrong token (or other problems)')
         return None
     logging.debug(response)
-    logging.debug(f'Авторизован в чате как {response.get("nickname")}')
+    logging.info(f'Authorised as: {response.get("nickname")}')
     return response.get('nickname')
 
 
 async def register(reader: StreamReader, writer: StreamWriter, nickname: str) -> str:
-    """Регистрация нового пользователя в чате."""
-    logging.debug('Пробуем зарегистрироваться')
+    """New user registration."""
+    logging.debug('Trying to register')
     greetings = await read_line_from_chat(reader)
-    logging.debug(f'Приветствие чата: {greetings}')
+    logging.debug(f'Greetings from the chat: {greetings}')
     await write_line_to_chat(writer, '')  # говорим, что у нас нет токена
     ask_for_nickname = await read_line_from_chat(reader)
-    logging.debug(f'Предложение выбрать свой никнейм: {ask_for_nickname}')
+    logging.debug(f'Chat asks about nickname: {ask_for_nickname}')
     await write_line_to_chat(writer, nickname)
     response_content = await read_line_from_chat(reader)
     if not response_content:
-        logging.error('Не удалось зарегистрироваться, попробуйте позднее')
+        logging.error('Registration failed. Please try again later.')
         return ''
     response = json.loads(response_content)
     logging.debug(response)
-    logging.debug(f'Зарегистрирован как {response.get("nickname")}')
-    logging.debug(f'Новый токен {response.get("account_hash")}')
+    logging.debug(f'Registered as {response.get("nickname")}')
+    logging.debug(f'New access token {response.get("account_hash")}')
     return response.get('account_hash')
